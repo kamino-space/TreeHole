6555cd8f8f1938f54760b6f347c95677b9a232d4
kamino <lishichenjs@163.com>
2019-04-09 23:13:46 +0800

    更新Websocket

--- CqApi/consumers.py
+++ CqApi/consumers.py
@@ -9,9 +9,10 @@ class ApiConsumer(WebsocketConsumer):
 
     def connect(self):
         self.accept()
-        while True:
-            if Core.SQ.empty():
-                continue
+        while Core.SQ.empty() == False:
             message = Core.SQ.get_nowait()
-            print("SEND %s" % message)
+            print("API SEND %s" % message)
             self.send(text_data=Core.MsgBuilder.send_private_msg(1019728153, message))
+
+    def receive(self, text_data=None, bytes_data=None):
+        print("API RETURN %s" % text_data)
